dcd11967cc5be019e6c22663a41fba9c64abfc1f
John Coleman <jcoleman@pagerduty.com>
2018-05-27 08:44:38 -0700

    added word list to dictionary

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -12,7 +12,19 @@ defmodule Dictionary do
       :world
 
   """
-  def hello do
-    :world
+  def name do
+    IO.puts "elixir for programmers method!"
+  end
+
+  def random_word do
+    word_list()
+    |> Enum.random()
+  end
+
+  def word_list do
+    "./assets/words.txt"
+    |> File.read()
+    |> elem(1)
+    |> String.split(~r/\n/)
   end
 end
