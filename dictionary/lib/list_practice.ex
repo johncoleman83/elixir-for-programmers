b1a7ecf811af5508e1096bdb6707e67d6ad5972d
John Coleman <jcoleman@pagerduty.com>
2018-05-28 23:49:01 -0700

    added more methods

--- dictionary/lib/list_practice.ex
+++ dictionary/lib/list_practice.ex
@@ -1,11 +1,20 @@
 defmodule Lists do
 
   def len( [] ), do: 0
-
-  def sum( [] ), do: 0
-
   def len([ head | tail]), do: 1 + len(tail)
 
+  def sum( [] ), do: 0
   def sum([ head | tail]), do: head + sum(tail)
+
+  def double([]), do: []
+  def double([ head | tail ]), do: [ 2*head | double(tail) ]
+
+  def square([]), do: []
+  def square([ head | tail ]), do: [ head*head | square(tail) ]
+
+  def sum_pairs([]), do: []
+  def sum_pairs([ h1, h2 | t]), do: [ h1 + h2 | sum_pairs(t) ]
+
+  def even_length?(alist), do: rem(len(alist), 2) == 0 || false
 
 end
